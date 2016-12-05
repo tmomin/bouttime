ffaadecbf1abb945cacf518c19a9201c624c5b42
Tanveer Momin <tmomin@Tanveers-Mac.local>
2016-12-04 23:49:42 -0600

    Instructions with Start button

--- Bout TIme/ViewController.swift
+++ Bout TIme/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        startButton.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
